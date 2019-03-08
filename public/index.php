726b9062bd12819faf0b19bafe29965751e0412f
inghamn <inghamn@bloomington.in.gov>
2019-03-08 11:26:02 -0500

    Added ansible deployment
    Updates #4

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @copyright 2015-2018 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @copyright 2019 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 /**
  * Grab a timestamp for calculating process time
